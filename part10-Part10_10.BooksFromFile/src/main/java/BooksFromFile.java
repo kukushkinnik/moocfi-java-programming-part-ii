0f516e714aa56dc657906c070289b3c5517a6fbe
Nik <kukushkinnika@gmail.com>
2021-12-23 19:09:30 -0500

    Finished Part10_10

---
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -0,0 +1,32 @@
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class BooksFromFile {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // test your method here
+
+    }
+
+    public static List<Book> readBooks(String file) {
+        List<Book> listOfBooks = new ArrayList<>();
+
+        try{
+            Files.lines(Paths.get(file))
+                    .map(row -> row.split(","))
+                    .filter(parts -> parts.length == 4)
+                    .map(parts -> new Book(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), parts[3]))
+                    .forEach(book -> listOfBooks.add(book));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return listOfBooks;
+    }
+
+}
