a88ad17333807f2174f353852cd46933b113a6d3
Nik <kukushkinnika@gmail.com>
2021-12-31 15:38:36 -0500

    Finished part11_13

--- part11-Part11_13.SaveableDictionary/src/main/java/dictionary/Main.java
+++ part11-Part11_13.SaveableDictionary/src/main/java/dictionary/Main.java
@@ -2,17 +2,9 @@ package dictionary;
 
 public class Main {
     public static void main(String[] args) {
-        SaveableDictionary dictionary = new SaveableDictionary();
-        dictionary.add("apina", "monkey");
-        dictionary.add("banaani", "banana");
-        dictionary.add("ohjelmointi", "programming");
-        dictionary.delete("apina");
-        dictionary.delete("banana");
-
-        System.out.println(dictionary.translate("apina"));
-        System.out.println(dictionary.translate("monkey"));
-        System.out.println(dictionary.translate("banana"));
-        System.out.println(dictionary.translate("banaani"));
-        System.out.println(dictionary.translate("ohjelmointi"));
+        SaveableDictionary s = new SaveableDictionary("word.txt");
+        s.load();
+        System.out.println();
+        s.save();
     }
 }
--- part11-Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
+++ part11-Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
@@ -1,14 +1,55 @@
 package dictionary;
 
+import java.io.FileWriter;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 
 public class SaveableDictionary {
     private HashMap<String, String> dictonary;
+    private String fileName;
 
     public SaveableDictionary() {
         this.dictonary = new HashMap<>();
+    }
+
+    public SaveableDictionary(String file) {
+        this();
+         this.fileName = file;
+    }
+
+    public boolean load() {
+        try(Scanner fileReader =  new Scanner(Paths.get(this.fileName))) {
+            while(fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] parts = line.split(":");
+                this.add(parts[0], parts[1]);
+            }
+        } catch (Exception e) {
+            return  false;
+        }
+        return true;
+    }
+
+    public boolean save()  {
+        try( FileWriter fileWriter = new FileWriter(this.fileName)) {
+            String key = "";
+            String value = "";
+
+            Iterator<Map.Entry<String, String>> iterator = this.dictonary.entrySet().iterator();
+            while(iterator.hasNext()) {
+                Map.Entry<String, String> entry = iterator.next();
+                key = entry.getKey();
+                value = entry.getValue();
+                fileWriter.write(key + ":" + value +"\n");
+            }
+
+        }catch (Exception e) {
+            return  false;
+        }
+       return true;
     }
 
     public void add(String word, String translation) {
@@ -16,7 +57,6 @@ public class SaveableDictionary {
     }
 
     public String translate(String word) {
-
         for (Map.Entry<String, String> entry : this.dictonary.entrySet()) {
             if (entry.getValue().equals(word)) {
                 return entry.getKey();
